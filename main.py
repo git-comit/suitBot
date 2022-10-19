555e4734aa079b3f944fd446f4a2c288c145f270
Git Comit <git.comit@gmail.com>
2022-10-19 11:30:09 -0400

    wip

--- main.py
+++ main.py
@@ -34,10 +34,6 @@ outfits = ["cape", "blue", "daovote", "ghost", "horns",
 # Search for the pfp id in the JSON dictionary and return the image URL associated with that id. You'll need to update the keys to match what's in your JSON delattr
 
 # Need to add error handling
-def print_data(id):
-    while i <= 20:
-        print(pfp_atts)
-        i+=1
 
 def get_pfp_img_url(id):
     for pfp in pfp_atts:
@@ -81,11 +77,10 @@ def deleteDressed(fit, pfp_id):
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+    print(pfp_atts)
 
 # !newfit command executes the get_dressed function and returns the resulting image. It accepts a value between 1 and 5000. Update this to use the command name you want and the values to fit the range of your project
-@bot.command(name='showdata')
-async def show_data(pfp_id):
-    print_data(pfp_id)
+
 
 @bot.command(name="newfit", brief='Dress your pfp', description='This command will let you apply new fits to your pfp')
 async def newfit(ctx, fit: str, pfp_id: int):
