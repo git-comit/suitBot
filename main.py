12cd4f8e2a270c664a09614e051d070ffa40931b
Britt Preyer <brittpreyer@Britts-MacBook-Air.local>
2022-11-30 21:29:35 -0500

    wip

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ def get_dressed(fit, pfp_id):
 def make_wallpaper(wallpaper, pfp_id):
     background = Image.open(wallpaper_folder + wallpaper.lower() + '.png')
     monke = Image.open(no_background_folder + pfp_id + '.png')
-    monke.resize((1920,1920))
-    background.paste(monke, (0, 0, 1920, 1920), mask=monke)
+    # monke.resize((1920,1920))
+    background.paste(monke, (0, 1920), mask=monke)
     background.save(save_img_folder + wallpaper.lower() + str(pfp_id) + '.png')
 
     return
