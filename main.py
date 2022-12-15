3e189ca1bf34ca6700910fcff71d9629c9a1f401
Git Comit <git.comit@gmail.com>
2022-12-15 10:30:13 -0500

    wip

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def brero_no_background(fit, pfp_id):
 
     return
 
-def pfp_background(background, fit, pfp_id):
+def pfp_background(background, pfp_id, fit):
     pfp= Image.open(pfp_background_folder + background.lower() + '.png')
     monke = Image.open(no_background_folder + str(pfp_id) + '.png')
     outfit = Image.open(outfits_folder + fit.lower() + '.png')
@@ -340,11 +340,11 @@ async def hqnb(ctx, pfp_id: int):
         # await ctx.send('Something went wrong')
 
 @bot.command(name='holiday', breif='Holiday monkes', description='backgrounds and outfits for monkes \n takes 2 commands background first \n ie `holiday blue elf 4470` \n use `none` for second command to return without costume')
-async def holiday(ctx, background: str, fit: str, pfp_id: int):
+async def holiday(ctx, background: str, pfp_id: int, fit: str):
     if background.lower() in pfp_backgrounds:
         if fit.lower() in outfits:
             if 0 < pfp_id <= 5000:
-                pfp_background(background, fit, pfp_id)
+                pfp_background(background,pfp_id, fit)
                 await ctx.send(file=discord.File(save_img_folder + background.lower() + str(pfp_id) + '.png'))
                 deleteDressed(background, str(pfp_id))
 
