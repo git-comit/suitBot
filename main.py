6ff6d8a5b7cc844c8134db454252f809a98d0e8c
Git Comit <git.comit@gmail.com>
2023-04-20 13:10:35 -0400

    wip

--- main.py
+++ main.py
@@ -642,7 +642,18 @@ async def holiday(ctx, background: str, pfp_id: int):
 async def banner(ctx, fit: str, pfp_id: int, pfp2=None, pfp3=None, pfp4=None, pfp5=None):
     if fit.lower() in banners:
         if 0 < pfp_id <= 5001:
-            make_banner(fit, str(pfp_id))
+            if pfp2:
+                make_banner(fit, str(pfp_id), str(pfp2))
+            if pfp3:
+                make_banner(fit, str(pfp_id), str(pfp2), str(pfp3))
+            if pfp4:
+                make_banner(fit, str(pfp_id), str(pfp2), str(pfp3), str(pfp4))
+            if pfp5:
+                make_banner(fit, str(pfp_id), str(pfp2),
+                            str(pfp3), str(pfp4), str(pfp5))
+            else:
+                make_banner(fit, str(pfp_id))
+
             await ctx.send(file=discord.File(save_img_folder + fit.lower() + str(pfp_id) + '.png'))
             deleteDressed(str(pfp_id))
 
